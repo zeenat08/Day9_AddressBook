07fa46be0aafca4a7bca9f3c1d06bf818d0700b7
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-25 08:25:26 +0530

    ZEENAT: Added main method of address book

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DAY9_Address_Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1_Details.AddPersonsDetails();
+            Console.WriteLine("Welcome To Address Book Programm");
+            Console.WriteLine("\n\n");
+            #region
+            UC1_Details.AddPersonsDetails();
+            #endregion
+
+
+            #region
+            UC2_Add_Contacts.NewContact();
+            UC2_Add_Contacts.ListPeople();
+            UC2_Add_Contacts.AddNew();
+            #endregion
+
+            #region
+            UC3_UpdateExistingContacts.NewContact();
+            UC3_UpdateExistingContacts.ListAllContacts();
+            UC3_UpdateExistingContacts.Update();
+            //List<string>[UC3_UpdateExistingContacts.People].FirstName.equals(firstname)
+            #endregion
+
+            #region
+            UC4_DeleteContact.NewContact();
+            UC4_DeleteContact.ListContacts();
+            UC4_DeleteContact.RemovePerson();
+            #endregion
+
+            #region
+            UC5_AddMultipleContacts.NewContact();
+            UC5_AddMultipleContacts.ListPeople();
+            UC5_AddMultipleContacts.AddNew();
+            #endregion
+
+            #region
+            UC6_UniqueAddress.AddUniqueAddress();
+            UC6_UniqueAddress.ListAllPeople();
+            UC6_UniqueAddress.ContactDetails();
+            #endregion
+
+
+        }
+    }
+}
