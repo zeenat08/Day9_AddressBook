a6387a9cc6be2aa997f02df240a933822333b21d
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-05 00:47:05 +0530

    ZEENAT:[added] update existing contacts in address book

--- UC3_UpdateExistingContacts.cs
+++ UC3_UpdateExistingContacts.cs
@@ -6,140 +6,175 @@ namespace DAY9_Address_Book
 {
     class UC3_UpdateExistingContacts
     { 
-          public string firstname;
-          public string lastname;
-          public string address;
-          public string city;
-          public string state;
-          public string zipcode;
-          public string phonenumber;
-          public string email;
+         
+        public string firstname;
+        public string lastname;
+        public string address;
+        public string city;
+        public string state;
+        public string zipcode;
+        public string phonenumber;
+        public string email;
 
-          public static List<UC3_UpdateExistingContacts> People = new List<UC3_UpdateExistingContacts>();
+        public static List<UC3_UpdateExistingContacts> People = new List<UC3_UpdateExistingContacts>();
 
-          public static void NewContact()
-          {
-                UC3_UpdateExistingContacts person = new UC3_UpdateExistingContacts();
+        public static void NewContact()
+        {
+            UC3_UpdateExistingContacts person = new UC3_UpdateExistingContacts();
 
-                Console.Write("Enter First Name: ");
-                person.firstname = Console.ReadLine();
+            Console.Write("Enter First Name: ");
+            person.firstname = Console.ReadLine();
 
-                Console.Write("Enter Last Name: ");
-                person.lastname = Console.ReadLine();
+            Console.Write("Enter Last Name: ");
+            person.lastname = Console.ReadLine();
 
-                Console.Write("Enter the Address: ");
-                person.address = Console.ReadLine();
+            Console.Write("Enter the Address: ");
+            person.address = Console.ReadLine();
 
-                Console.Write("Enter City name: ");
-                person.city = Console.ReadLine();
+            Console.Write("Enter City name: ");
+            person.city = Console.ReadLine();
 
-                Console.Write("Enter State name: ");
-                person.state = Console.ReadLine();
+            Console.Write("Enter State name: ");
+            person.state = Console.ReadLine();
 
-                Console.Write("Enter the Zip Code: ");
-                person.zipcode = Console.ReadLine();
+            Console.Write("Enter the Zip Code: ");
+            person.zipcode = Console.ReadLine();
 
-                Console.Write("Enter Phone Number: ");
-                person.phonenumber = Console.ReadLine();
+            Console.Write("Enter Phone Number: ");
+            person.phonenumber = Console.ReadLine();
 
-                Console.Write("Enter the Email ID: ");
-                person.email = Console.ReadLine();
-
-
-                People.Add(person);
-            }
+            Console.Write("Enter the Email ID: ");
+            person.email = Console.ReadLine();
 
 
-            public static void PrintingDetails(UC3_UpdateExistingContacts person)
+            People.Add(person);
+        }
+
+
+        public static void PrintingDetails(UC3_UpdateExistingContacts person)
+        {
+            Console.WriteLine("First Name: " + person.firstname);
+            Console.WriteLine("Last Name: " + person.lastname);
+            Console.WriteLine("Address: " + person.address);
+            Console.WriteLine("City: " + person.city);
+            Console.WriteLine("State: " + person.state);
+            Console.WriteLine("Zip Code: " + person.zipcode);
+            Console.WriteLine("Phone Number: " + person.phonenumber);
+            Console.WriteLine("Email ID: " + person.email);
+            Console.WriteLine("-------------------------------------------");
+        }
+
+        public static void ListAllContacts()
+        {
+            if (People.Count == 0)
             {
-                Console.WriteLine("First Name: " + person.firstname);
-                Console.WriteLine("Last Name: " + person.lastname);
-                Console.WriteLine("Address: " + person.address);
-                Console.WriteLine("City: " + person.city);
-                Console.WriteLine("State: " + person.state);
-                Console.WriteLine("Zip Code: " + person.zipcode);
-                Console.WriteLine("Phone Number: " + person.phonenumber);
-                Console.WriteLine("Email ID: " + person.email);
-                Console.WriteLine("-------------------------------------------");
-            }
-
-            public static void ListAllContacts()
-            {
-                if (People.Count == 0)
-                {
-                    Console.WriteLine("Your address book is empty. Press any key to continue.");
-                    Console.ReadKey();
-                    return;
-                }
-                Console.WriteLine("Here are the current people in your address book:\n");
-                foreach (var person in People)
-                {
-                    PrintingDetails(person);
-                }
-                Console.WriteLine("\nPress any key to continue.");
+                Console.WriteLine("Your address book is empty. Press any key to continue.");
                 Console.ReadKey();
-
+                return;
             }
-
-            public static void Update()
+            Console.WriteLine("Here are the current people in your address book:\n");
+            foreach (var person in People)
             {
-                Console.WriteLine("Press 1 If you want to edit any Contact in the Address Book");
-                switch (Console.ReadLine())
-                {
-                    case "1":
-                        Console.WriteLine("Enter the First Name of the Person U want to update");
-                        string firstname = Console.ReadLine();
-                        UC3_UpdateExistingContacts findperson = People.Find(x => x.firstname.ToLower() == firstname.ToLower());
-                        if (findperson == null)
+                PrintingDetails(person);
+            }
+            Console.WriteLine("\nPress any key to continue.");
+            Console.ReadKey();
+
+        }
+
+        public static void Update()
+        {
+            Console.WriteLine("Press 1 If you want to edit any Contact in the Address Book");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine("Enter the First Name of the Person U want to update");
+                    string firstname = Console.ReadLine();
+                    UC3_UpdateExistingContacts person = People.Find(x => x.firstname.ToLower() == firstname.ToLower());
+                    if (person == null)
+                    {
+                        Console.WriteLine("That person U entered is not found");
+                    }
+
+                    Console.WriteLine("Are you sure you want to edit the person details from your address book? Enter --> (Y/N)");
+                    PrintingDetails(person);
+                    if (Console.ReadKey().Key == ConsoleKey.Y)
+                    {
+                        Console.WriteLine("Press 1 if u want to edit the LastName");
+                        Console.WriteLine("Press 2 if u want to edit the Address");
+                        Console.WriteLine("Press 3 if u want to edit the City");
+                        Console.WriteLine("Press 4 if u want to edit the State");
+                        Console.WriteLine("Press 5 if u want to edit the Zip Code");
+                        Console.WriteLine("Press 6 if u want to edit the Phone Number");
+                        Console.WriteLine("Press 7 if u want to edit the Email Id");
+                        int choice = Convert.ToInt32(Console.ReadLine());
+                        switch (choice)
                         {
-                            Console.WriteLine("That person could not be found");
+                            case 1:
+
+                                Console.WriteLine("Enter New LastName; ");
+                                person.lastname = Console.ReadLine();
+                                People.Add(person);
+                                Console.WriteLine("Last Name: " + person.lastname);
+                                break;
+                            case 2:
+
+                                Console.WriteLine("Enter New Address; ");
+                                person.address = Console.ReadLine();
+                                People.Add(person);
+                                Console.WriteLine("Address: " + person.address);
+                                break;
+                            case 3:
+
+                                Console.WriteLine("Enter New City: ");
+                                person.city = Console.ReadLine();
+                                People.Add(person);
+                                Console.WriteLine("City: " + person.city);
+                                break;
+                            case 4:
+
+                                Console.WriteLine("Enter New State: ");
+                                person.state = Console.ReadLine();
+                                People.Add(person);
+                                Console.WriteLine("MobileNumber: " + person.state);
+                                break;
+                            case 5:
+
+                                Console.WriteLine("Enter New ZipCode: ");
+                                person.zipcode = Console.ReadLine();
+                                People.Add(person);
+                                Console.WriteLine("ZipCode: " + person.zipcode);
+                                break;
+                            case 6:
+
+                                Console.WriteLine("Enter New PhoneNumber: ");
+                                person.phonenumber = Console.ReadLine();
+                                People.Add(person);
+                                Console.WriteLine("PhoneNumber: " + person.phonenumber);
+                                break;
+                            case 7:
+
+                                Console.WriteLine("Enter New Email ID: ");
+                                person.email = Console.ReadLine();
+                                People.Add(person);
+                                Console.WriteLine("Email Id: " + person.email);
+                                break;
+
+                            default:
+                                Console.WriteLine("Invalid Choice");
+                                break;
                         }
-
-                        Console.WriteLine("Are you sure you want to remove this person and enter new in your address book? Enter --> (Y/N)");
-                        PrintingDetails(findperson);
-                        if (Console.ReadKey().Key == ConsoleKey.Y)
-                        {
-                            People.Remove(findperson);
-                            Console.WriteLine("Person removed. Press any key to continue.");
-                            Console.ReadKey();
-                        }
-                        if (Console.ReadKey().Key == ConsoleKey.N)
-                        {
-                            Console.WriteLine("OKK. Press any key to continue.");
-                        }
-
-                        ListAllContacts();
-
-                        //NewContact();
-                        //for (int i = 0; i < People.Count; i++)
-                        //{
-                        //    if (People[i].firstname.Equals(firstname))
-                        //    {
-                        //        Console.WriteLine("Press 1 if u want to edit the FirstName");
-                        //        Console.WriteLine("Press 2 if u want to edit the LastName");
-                        //        Console.WriteLine("Press 3 if u want to edit the Address");
-                        //        Console.WriteLine("Press 4 if u want to edit the City");
-                        //        Console.WriteLine("Press 5 if u want to edit the State");
-                        //        Console.WriteLine("Press 6 if u want to edit the Zip Code");
-                        //        Console.WriteLine("Press 7 if u want to edit the Phone Number");
-                        //        Console.WriteLine("Press 8 if u want to edit the Email Id");
-                        //        switch(Console.ReadLine())
-                        //        {
-                        //            case "1":
-                        //                Console.Write("Enter new FirstName: ");
-                        //                break;
-                        //        }
-                        //    }
-                        //}
-
-
-                        break;
-                    default:
-                        Console.WriteLine("The choice you made is not valid, please try again");
-                        break;
-                }
+                    }
+                    if (Console.ReadKey().Key == ConsoleKey.N)
+                    {
+                        Console.WriteLine("OKK. Press any key to continue.");
+                    }
+                    ListAllContacts();
+                    break;
+                default:
+                    Console.WriteLine("Thanku!!!");
+                    break;
             }
         }
     }
-
-   
+}
